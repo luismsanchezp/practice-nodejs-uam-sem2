8dcdc8c1239ec2d26e975cb1c7eb5cd33962a744
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2022-08-25 08:34:35 -0500

    Add models and routes for person and course

---
+++ src/models/course.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const courseSchema = mongoose.Schema({
+    course_name:{type: String, require: true},
+    code:{type: String, require: true, unique: true},
+    credits:{type: Number, require: true},
+    hours:{type: Number, require: true},
+});
+module.exports = mongoose.model('CourseCollection', courseSchema);
