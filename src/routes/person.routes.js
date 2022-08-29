d8031dcf2ab7364652078448c2dd251ca2a78420
luismsanchezp <luism.sanchezp@autonoma.edu.co>
2022-08-29 17:00:02 -0500

    Add new request methods in person.routes.js

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -9,8 +9,21 @@ const person_routes = express.Router(req, res)
     PUT{id}: update existent record
     DELETE{id}: delete existent record
 */
-person_routes.get('/', (req, res)=>{})
+person_routes.get('/', (req, res)=>{
+    person_model
+        .find()
+        .then((data) => res.json(data))
+        .catch((err) => res.json(err));
+});
 person_routes.get('/personId', (req, res)=>{})
-person_routes.post('/', (req, res)=>{})
+person_routes.post('/', (req, res)=>{
+    const new_person = person_model(req.body);
+    new_person
+        .save()
+        .then((data) => res.json(data))
+        .catch((err) => res.json(err));
+});
 person_routes.put('/personId', (req, res)=>{})
 person_routes.delete('/personId', (req, res)=>{})
+
+module.exports = person_routes
